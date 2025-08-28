47d8760898c77e78cd8dc7acc4b7e43b3efa7816
emid1-o <emidiolop5@gmail.com>
2025-08-28 14:47:48 -0300

    spring security começo

--- src/main/java/br/com/movieflix/service/UserService.java
+++ src/main/java/br/com/movieflix/service/UserService.java
@@ -3,6 +3,7 @@ package br.com.movieflix.service;
 import br.com.movieflix.entity.User;
 import br.com.movieflix.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,12 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User save(User user){
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
         return userRepository.save(user);
     }
 }
